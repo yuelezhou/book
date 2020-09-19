64685d1b054e04cbe356f6160452ca4978e50099
Zhou Le <yuelezhou@163.com>
2020-09-19 17:14:03 +0800

    0919 测试

---
+++ effectivejava/src/main/java/com/book/effectivejava/chap6/issue34/Issue34.java
@@ -0,0 +1,45 @@
+package com.book.effectivejava.chap6.issue34;
+
+
+/**
+ * @ Author     ：zhoule
+ * @ Date       ：Created in 16:56 2020-09-19
+ * @ Description：
+ * @ Modified By：
+ * @ Version:
+ */
+public class Issue34 {
+
+    public enum Chinese {
+        WANGPENG(1);
+
+        private Integer sex;
+
+        Chinese(Integer sex) {
+            this.sex = sex;
+        }
+
+        public Integer getSex() {
+            return sex;
+        }
+    }
+
+    public enum Japanese {
+        WANGPENG(1);
+        private Integer sex;
+
+        Japanese(Integer sex) {
+            this.sex = sex;
+        }
+
+        public Integer getSex() {
+            return sex;
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean flag = Chinese.WANGPENG.equals(Japanese.WANGPENG);
+        assert flag : "flag is true";
+        System.out.println("断言通过");
+    }
+}
